6b9765d9a8b4bef43bd2730258ce675e7a8a1bce
Juan Esteban Valverde Mora <valverdemorajuanesteban@gmail.com>
2024-11-14 15:50:33 -0600

    Subir programa al nuevo repositorio
    Contenido subido: Clases, JFrames y codigo para que funcionen.

---
+++ FormularioRegistro/src/Modelos/Datos.java
@@ -0,0 +1,72 @@
+
+package Modelos;
+
+import Modelos.Personas;
+import VistaInterfaz.Administradores;
+import VistaInterfaz.Inicio;
+import VistaInterfaz.Login;
+import VistaInterfaz.Registro;
+import VistaInterfaz.Usuarios;
+import java.util.ArrayList;
+
+
+public class Datos {
+   
+    
+   public static ArrayList<Personas> listaPersonas = new ArrayList<Personas>();
+   public static Administradores admins = new Administradores();
+   public static Registro registro = new Registro();
+   public static Login login = new Login();
+   public static Inicio start = new Inicio();
+   public static Usuarios users = new Usuarios();
+
+    public static Usuarios getUsers() {
+        return users;
+    }
+
+    public static void setUsers(Usuarios users) {
+        Datos.users = users;
+    }
+
+    public static ArrayList<Personas> getListaPersonas() {
+        return listaPersonas;
+    }
+
+    public static void setListaPersonas(ArrayList<Personas> listaPersonas) {
+        Datos.listaPersonas = listaPersonas;
+    }
+
+    public static Administradores getAdmins() {
+        return admins;
+    }
+
+    public static void setAdmins(Administradores admins) {
+        Datos.admins = admins;
+    }
+
+    public static Registro getRegistro() {
+        return registro;
+    }
+
+    public static void setRegistro(Registro registro) {
+        Datos.registro = registro;
+    }
+
+    public static Login getLogin() {
+        return login;
+    }
+
+    public static void setLogin(Login login) {
+        Datos.login = login;
+    }
+
+    public static Inicio getStart() {
+        return start;
+    }
+
+    public static void setStart(Inicio start) {
+        Datos.start = start;
+    }
+   
+   
+}
---
+++ FormularioRegistro/src/Modelos/Personas.java
@@ -0,0 +1,75 @@
+
+package Modelos;
+
+
+public class Personas {
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String cedula;
+    private int rol;
+    private String password;
+
+    public Personas(String nombre, String apellido, String telefono, String cedula, String password, int rol) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.cedula = cedula;
+        this.rol = rol;
+        this.password = password;
+    }
+    public Personas(){
+        
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public int getRol() {
+        return rol;
+    }
+
+    public void setRol(int rol) {
+        this.rol = rol;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+    
+}
