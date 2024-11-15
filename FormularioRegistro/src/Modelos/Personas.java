2ac4685a24fd52289d0b8acf0a242edf82e0d650
Juan Esteban Valverde Mora <valverdemorajuanesteban@gmail.com>
2024-11-15 14:49:38 -0600

    PruebaDeRespuesta
    probe que se actualizaran los cambios

--- FormularioRegistro/src/Modelos/Personas.java
+++ FormularioRegistro/src/Modelos/Personas.java
@@ -9,7 +9,7 @@ public class Personas {
     private String cedula;
     private int rol;
     private String password;
-
+    
     public Personas(String nombre, String apellido, String telefono, String cedula, String password, int rol) {
         this.nombre = nombre;
         this.apellido = apellido;
